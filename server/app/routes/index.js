679fd6cdcc063565c8da53770a60a93ea6fb62ca
NastiaS <anastasiasergienko@gmail.com>
2015-04-30 16:17:45 -0400

    Yelp route working

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,6 +4,7 @@ module.exports = router;
 
 router.use('/tutorial', require('./tutorial'));
 router.use('/members', require('./members'));
+router.use('/yelp', require('./yelp'));
 
 // Make sure this is after all of
 // the registered routes!
